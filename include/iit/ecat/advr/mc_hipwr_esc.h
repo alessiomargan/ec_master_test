305dffffd1fa98faf88d919de9ad5edf70f9c9fa
MirkoFerrati <mirko.ferrati@gmail.com>
2014-12-18 01:48:02 +0100

    low level pdos are now in their own header files

--- include/iit/ecat/advr/mc_hipwr_esc.h
+++ include/iit/ecat/advr/mc_hipwr_esc.h
@@ -18,178 +18,20 @@
 #include <iit/ecat/advr/esc.h>
 
 #include <iit/ecat/advr/pipes.h>
-
+#include <json_serialization.hpp>
+#include <iit/ecat/advr/types.h>
 #include <json/json.h>
+#include <map>
 
 namespace iit {
 namespace ecat {
 namespace advr {
 
-#define MAP_ITEM(x) [#x] = std::to_string(x)
-
 typedef std::map<std::string, std::string> pdo_map_t;
-
-#define params(x) #x,x
-
-inline void add_object(std::string name, float value, json_object* jobj)
-{
-    json_object *jfloat  = json_object_new_double(value);
-    json_object_object_add(jobj,name.c_str(),jfloat);
-}
-
-inline void add_object(std::string name, uint64_t value, json_object* jobj)
-{
-    json_object *jn  = json_object_new_int64(value);
-    json_object_object_add(jobj,name.c_str(),jn);
-}
-
-inline void add_object(std::string name, uint16_t value, json_object* jobj)
-{
-    json_object *jn  = json_object_new_int(value);
-    json_object_object_add(jobj,name.c_str(),jn);
-}
-
-inline void add_object(std::string name, long value, json_object* jobj)
-{
-    json_object *jn  = json_object_new_int(value);
-    json_object_object_add(jobj,name.c_str(),jn);
-}
-
 
 /**
  *  
  **/ 
-
-struct McESCTypes {
-    // TX  slave_input -- master output
-    typedef struct {
-        float	    pos_ref;
-        float		tor_ref;
-        float		direct_ref;
-        uint64_t	ts;
-
-        void serializeToJson(json_object* jObj);
-        void deSerializeToJson(json_object *jObj);        
-
-    }  /*__attribute__((__packed__))*/ pdo_tx;
-
-    // RX  slave_output -- master input
-    typedef struct {
-        float	    position;   // rad
-        float		velocity;   // rad/s
-        float		torque;     // Nm
-        float		torque_D;   // Nm/s
-        float		direct_out; // A in AC or V in DC
-        uint16_t    fault;
-        uint64_t	rtt;        // ns 
-
-        void serializeToJson(json_object* jObj);
-        void deSerializeToJson(json_object *jObj);  
-
-    }  /*__attribute__((__packed__))*/ pdo_rx;
-};
-
-
-
-inline void McESCTypes::pdo_tx::serializeToJson(json_object* jObj)
-{
-    /*
-    float        pos_ref;
-    float        tor_ref;
-    float        direct_ref;
-    uint64_t    ts;
-    */
-
-    add_object(params(pos_ref),jObj);
-    add_object(params(tor_ref),jObj);
-    add_object(params(direct_ref),jObj);
-    add_object(params(ts),jObj);
-    
-}
-
-inline void McESCTypes::pdo_rx::serializeToJson(json_object* jObj)
-{
-    /*
-    float        position;   // rad
-    float        velocity;   // rad/s
-    float        torque;     // Nm
-    float        torque_D;   // Nm/s
-    float        direct_out; // A in AC or V in DC
-    uint16_t    fault;
-    uint64_t    rtt;        // ns 
-    */
-
-    add_object(params(position),jObj);
-    add_object(params(velocity),jObj);
-    add_object(params(torque),jObj);
-    add_object(params(torque_D),jObj);
-    add_object(params(direct_out),jObj);
-    add_object(params(fault),jObj);
-    add_object(params(rtt),jObj);
-    
-}
-
-inline void get_object(std::string name, float &value, json_object* jobj)
-{
-    json_object *jfloat  = json_object_object_get(jobj,name.c_str());
-    value = json_object_get_double(jfloat);
-}
-
-inline void get_object(std::string name, uint64_t& value, json_object* jobj)
-{
-    json_object *jn  = json_object_object_get(jobj,name.c_str());
-    value = json_object_get_int64(jn);
-}
-
-inline void get_object(std::string name, uint16_t& value, json_object* jobj)
-{
-    json_object *jn  = json_object_object_get(jobj,name.c_str());
-    value = json_object_get_int(jn);
-}
-
-inline void get_object(std::string name, long& value, json_object* jobj)
-{
-    json_object *jn  = json_object_object_get(jobj,name.c_str());
-    value = json_object_get_int(jn);
-}
-  
-inline void McESCTypes::pdo_tx::deSerializeToJson(json_object* jObj)
-{
-    /*
-    float        pos_ref;
-    float        tor_ref;
-    float        direct_ref;
-    uint64_t    ts;
-    */
-
-    get_object(params(pos_ref),jObj);
-    get_object(params(tor_ref),jObj);
-    get_object(params(direct_ref),jObj);
-    get_object(params(ts),jObj);
-}
-
-inline void McESCTypes::pdo_rx::deSerializeToJson(json_object* jObj)
-{
-    /*
-    float        position;   // rad
-    float        velocity;   // rad/s
-    float        torque;     // Nm
-    float        torque_D;   // Nm/s
-    float        direct_out; // A in AC or V in DC
-    uint16_t    fault;
-    uint64_t    rtt;        // ns 
-    */
-
-    get_object(params(position),jObj);
-    get_object(params(velocity),jObj);
-    get_object(params(torque),jObj);
-    get_object(params(torque_D),jObj);
-    get_object(params(direct_out),jObj);
-    get_object(params(fault),jObj);
-    get_object(params(rtt),jObj);
-}
-
-
 
 class McESC : public BasicEscWrapper<McESCTypes>
 {
@@ -213,7 +55,7 @@ public:
     Read_XDDP_pipe *    xddp_rd;
 
 private:
-
+    json_serializer serializer;
     pdo_map_t      mc_pdo_map;
 
 
---
+++ include/iit/ecat/advr/types.h
@@ -0,0 +1,41 @@
+#ifndef ECAT_TYPES_H
+#define ECAT_TYPES_H
+
+#include <map>
+namespace iit {
+    namespace ecat {
+        namespace advr {
+            
+            typedef std::map<std::string, std::string> pdo_map_t;
+            
+            /**
+             *  
+             **/ 
+            
+            struct McESCTypes {
+                // TX  slave_input -- master output
+                typedef struct {
+                    float       pos_ref;
+                    float           tor_ref;
+                    float           direct_ref;
+                    uint64_t        ts;
+                    
+                }  __attribute__((__packed__)) pdo_tx;
+                
+                // RX  slave_output -- master input
+                typedef struct {
+                    float       position;   // rad
+                    float           velocity;   // rad/s
+                    float           torque;     // Nm
+                    float           torque_D;   // Nm/s
+                    float           direct_out; // A in AC or V in DC
+                    uint16_t    fault;
+                    uint64_t        rtt;        // ns 
+                    
+                }  __attribute__((__packed__)) pdo_rx;
+            };
+}
+}
+}
+
+#endif
