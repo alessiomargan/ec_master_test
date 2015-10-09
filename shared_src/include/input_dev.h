a02c36efab216d2e48f37c6a1b4fd3383647ea54
Alessio Margan <alessio.margan@iit.it>
2015-10-09 10:29:12 +0200

    create examples dir and move files

---
+++ shared_src/include/input_dev.h
@@ -0,0 +1,37 @@
+#ifndef __INPUT_DEVICE_H__
+#define __INPUT_DEVICE_H__
+
+#include <thread_util.h>
+
+class Joy_Thread :
+    public Thread_hook
+{
+public:
+    
+    Joy_Thread(const char * config_yaml) {
+	name = "EC_boards_joint_joy";
+	period.period = {0,0};
+	schedpolicy = SCHED_OTHER;
+	priority = sched_get_priority_max(schedpolicy);
+
+    }
+    
+    virtual ~Joy_Thread() {
+	 
+    }
+
+    virtual void th_init(void *) {
+	
+    }
+    
+    virtual void th_loop(void *) {
+	
+    }
+
+protected:
+    
+    int xddp_sock;
+    
+}
+
+#endif
