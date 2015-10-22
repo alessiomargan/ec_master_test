dac92a497e460fa4737d4d7ac24f68ef23d1c956
Alessio Margan <alessio.margan@iit.it>
2015-10-22 16:17:37 +0200

    add ati and fw_update form walkman-master-ecat and add thread_util from COMAN_shared

---
+++ examples/ati/ft_ati_test.cpp
@@ -0,0 +1,263 @@
+#include <signal.h>
+#include <sys/mman.h>
+#include <execinfo.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#ifdef __XENO__
+    #include <rtdk.h>
+#endif
+
+#include <boost/circular_buffer.hpp>
+#include <boost/tokenizer.hpp>
+
+#include <iit/ecat/advr/ec_boards_iface.h>
+#include <ati_iface.h>
+
+#define FT_ECAT_POS 1
+
+
+using namespace iit::ecat::advr;
+
+static int run_loop = 1;
+
+
+
+typedef struct {
+    uint64_t    ts;
+    float       ati[6];
+    float       iit[6];
+    float       dummy[6];
+    void sprint(char *buff, size_t size) {
+        snprintf(buff, size, "%lu\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t0\t0\t0\t0\t0\t0\n", ts,
+// big sensor
+                 iit[0],iit[1],iit[2],iit[3],iit[4],iit[5],
+                 -ati[0],ati[1],ati[2],-ati[3],ati[4],ati[5]);
+// small sensor
+//                 iit[0],iit[1],iit[2],iit[3],iit[4],iit[5],
+//                 ati[0],ati[1],ati[2],ati[3],ati[4],ati[5]);
+    }
+    void fprint(FILE *fp) {
+        fprintf(fp, "%lu\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t0\t0\t0\t0\t0\t0\n", ts,
+// big sensor
+                iit[0],iit[1],iit[2],iit[3],iit[4],iit[5],
+                -ati[0],ati[1],ati[2],-ati[3],ati[4],ati[5]);
+// small sensor
+//                iit[0],iit[1],iit[2],iit[3],iit[4],iit[5],
+//                ati[0],ati[1],ati[2],ati[3],ati[4],ati[5]);
+
+    }
+} sens_data_t ; 
+
+
+void load_matrix(std::string filename, std::vector<std::vector<float>> &cal_matrix) {
+
+    std::string     line;
+    std::fstream    file(filename, std::ios::in);
+
+    typedef boost::tokenizer<boost::char_separator<char>> Tokenizer;
+    boost::char_separator<char> sep(",");
+
+    if (file) {
+        
+        while (getline(file, line)) {
+
+            Tokenizer info(line, sep); // tokenize the line of data
+
+            std::vector<float> values;
+
+            for (Tokenizer::iterator it = info.begin(); it != info.end(); ++it) {
+                // convert data into double value, and store
+                values.push_back(atof(it->c_str()));
+            }
+            // store array of values
+            cal_matrix.push_back(values);
+        }
+
+    } else {
+        std::cerr << "Error: Unable to open file " << filename << std::endl;
+    }
+}
+
+static void warn_upon_switch(int sig __attribute__((unused)))
+{
+    // handle rt to nrt contex switch
+    void *bt[3];
+    int nentries;
+
+    /* Dump a backtrace of the frame which caused the switch to
+       secondary mode: */
+    nentries = backtrace(bt,sizeof(bt)/sizeof(bt[0]));
+    // dump backtrace 
+    backtrace_symbols_fd(bt,nentries,fileno(stdout));
+}
+
+static void shutdown(int sig __attribute__((unused)))
+{
+    run_loop = 0;
+    DPRINTF("got signal .... Shutdown\n");
+}
+
+static void set_signal_handler(void)
+{
+    signal(SIGINT, shutdown);
+    signal(SIGINT, shutdown);
+    signal(SIGKILL, shutdown);
+#ifdef __XENO__
+    // call pthread_set_mode_np(0, PTHREAD_WARNSW) to cause a SIGXCPU
+    // signal to be sent when the calling thread involontary switches to secondary mode
+    signal(SIGXCPU, warn_upon_switch);
+#endif
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace iit::ecat;
+
+Ec_Boards_ctrl * ec_boards_ctrl; 
+
+
+int main(int argc, char **argv)
+{
+    int ret;
+
+    boost::circular_buffer<sens_data_t> sens_log;
+    sens_log.set_capacity(LOG_SIZE);
+
+    set_signal_handler();
+
+#ifdef __XENO__
+
+    int policy = SCHED_FIFO;
+    struct sched_param  schedparam;
+    schedparam.sched_priority = sched_get_priority_max(policy);
+    pthread_setschedparam(pthread_self(), SCHED_FIFO, &schedparam);
+
+    /* Prevent any memory-swapping for this program */
+    ret = mlockall(MCL_CURRENT | MCL_FUTURE);
+    if ( ret < 0 ) {
+        printf("mlockall failed (ret=%d) %s\n", ret, strerror(ret));
+        return 0;
+    }
+    /*
+     * This is a real-time compatible printf() package from
+     * Xenomai's RT Development Kit (RTDK), that does NOT cause
+     * any transition to secondary (i.e. non real-time) mode when
+     * writing output.
+     */
+    rt_print_auto_init(1);
+#endif
+
+    if ( argc != 2 ) {
+        printf("Usage: %s ifname\nifname = {eth0,rteth0} for example\n", argv[0]);
+        return 0;
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+
+    Ati_Sens * ati = new Ati_Sens(true);
+
+    ///////////////////////////////////////////////////////////////////////
+
+    std::vector<std::vector<float>> cal_matrix;
+    load_matrix("RightFootCalibMatrix.txt", cal_matrix);
+    //load_matrix("LeftFootCalibMatrix.txt", cal_matrix);
+    //load_matrix("RightWristCalibMatrix.txt", cal_matrix);
+    //load_matrix("LeftWristCalibMatrix.txt", cal_matrix);
+    //load_matrix("ExtraWristCalibMatrix.txt", cal_matrix);
+    //load_matrix("ones.txt", cal_matrix);
+
+    std::vector<std::vector<float>>::const_iterator it = cal_matrix.begin();
+    while ( it != cal_matrix.end()) {
+        std::vector<float> row(*it);
+        std::vector<float>::const_iterator rit = row.begin();
+        while ( rit != row.end()) {
+            std::cout << (*rit) << "\t";
+            rit ++;
+        }
+        std::cout << std::endl;
+        it ++;
+    }
+    //return 0;
+
+    ///////////////////////////////////////////////////////////////////////
+
+    Ec_Boards_ctrl * ec_boards_ctrl;
+
+    ec_boards_ctrl = new Ec_Boards_ctrl(argv[1]); 
+
+    if ( ec_boards_ctrl->init() != EC_BOARD_OK) {
+        std::cout << "Error in boards init()... cannot proceed!" << std::endl;		
+        delete ec_boards_ctrl;
+        return 0;
+    }
+
+    ec_boards_ctrl->configure_boards();
+
+    Rid2PosMap  rid2pos = ec_boards_ctrl->get_Rid2PosMap();
+
+    int cnt = 0;
+
+    
+    ec_boards_ctrl->set_flash_cmd(FT_ECAT_POS, CTRL_REMOVE_TORQUE_OFFS);
+
+    ec_boards_ctrl->set_cal_matrix(FT_ECAT_POS, cal_matrix);
+    ec_boards_ctrl->set_flash_cmd(FT_ECAT_POS, 0x0012);
+
+    Ft6EscPdoTypes::pdo_rx ft_pdo_rx;
+    Ft6EscPdoTypes::pdo_tx ft_pdo_tx;
+
+    ati_log_t   sample;
+    sens_data_t sens_data;
+
+    if ( ec_boards_ctrl->set_operative() <= 0 ) {
+        std::cout << "Error in boards set_operative()... cannot proceed!" << std::endl; 
+        delete ec_boards_ctrl;
+        return 0;
+    }
+
+    //sleep(3);
+
+    uint64_t    start = get_time_ns();
+
+    while ( run_loop ) {
+
+        ec_boards_ctrl->recv_from_slaves();
+        ati->get_last_sample(sample);
+
+        if ( (cnt % 10) == 0 ) {
+            //ec_boards_ctrl->check_sanity();
+        }
+        cnt++;
+
+        ///////////////////////////////////////////////////////////////////////
+
+        ec_boards_ctrl->getRxPDO(FT_ECAT_POS, ft_pdo_rx);
+        //DPRINTF("IIT\n");
+        //ft_pdo_rx.fprint(stderr); 
+        //DPRINTF("ATI\n");
+        //sample.fprint(stderr);
+        //DPRINTF("\n");
+
+        sens_data.ts = get_time_ns() - start;
+        memcpy((void*)&sens_data.ati, &sample.ft, sizeof(float)*6);
+        memcpy((void*)&sens_data.iit, &ft_pdo_rx.force_X, sizeof(float)*6);
+        sens_log.push_back(sens_data);
+        sens_data.fprint(stderr);
+
+        ///////////////////////////////////////////////////////////////////////
+
+        ec_boards_ctrl->send_to_slaves();
+
+    }
+
+    delete ec_boards_ctrl;
+    delete ati;
+
+    /////////////////////////////////////////////////////////////////
+
+    std::string filename = "/tmp/sens.txt";
+    dump_buffer(filename, sens_log);
+
+    return 0;
+}
---
+++ examples/fw_update/firm_update.cpp
@@ -0,0 +1,242 @@
+#include <signal.h>
+#include <sys/mman.h>
+#include <execinfo.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <yaml-cpp/yaml.h>
+
+#ifdef __XENO__
+    #include <rtdk.h>
+#endif
+
+#include <memory>
+#include <numeric>
+
+#include <iit/ecat/advr/ec_boards_iface.h>
+
+
+using namespace iit::ecat::advr;
+
+static int run_loop = 1;
+
+static void warn_upon_switch(int sig __attribute__((unused)))
+{
+    // handle rt to nrt contex switch
+    void *bt[3];
+    int nentries;
+
+    /* Dump a backtrace of the frame which caused the switch to
+       secondary mode: */
+    nentries = backtrace(bt,sizeof(bt)/sizeof(bt[0]));
+    // dump backtrace 
+    backtrace_symbols_fd(bt,nentries,fileno(stdout));
+}
+
+static void shutdown(int sig __attribute__((unused)))
+{
+    run_loop = 0;
+    DPRINTF("got signal .... Shutdown\n");
+}
+
+static void set_signal_handler(void)
+{
+    signal(SIGINT, shutdown);
+    signal(SIGINT, shutdown);
+    signal(SIGKILL, shutdown);
+#ifdef __XENO__
+    // call pthread_set_mode_np(0, PTHREAD_WARNSW) to cause a SIGXCPU
+    // signal to be sent when the calling thread involontary switches to secondary mode
+    signal(SIGXCPU, warn_upon_switch);
+#endif
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace iit::ecat;
+
+Ec_Boards_ctrl * ec_boards_ctrl; 
+
+
+int main(int argc, char **argv)
+{
+    int ret;
+
+    set_signal_handler();
+
+#ifdef __XENO__
+    
+    int policy = SCHED_FIFO;
+    struct sched_param  schedparam;
+    schedparam.sched_priority = sched_get_priority_max(policy);
+    pthread_setschedparam(pthread_self(), SCHED_FIFO, &schedparam);
+
+    /* Prevent any memory-swapping for this program */
+    ret = mlockall(MCL_CURRENT | MCL_FUTURE);
+    if ( ret < 0 ) {
+        printf("mlockall failed (ret=%d) %s\n", ret, strerror(ret));
+        return 0;
+    }
+    /*
+     * This is a real-time compatible printf() package from
+     * Xenomai's RT Development Kit (RTDK), that does NOT cause
+     * any transition to secondary (i.e. non real-time) mode when
+     * writing output.
+     */
+    rt_print_auto_init(1);
+#endif
+
+
+    if ( argc != 2) {
+    printf("Usage: %s config.yaml\n", argv[0]);
+        return 0;
+    }
+
+
+    Ec_Boards_ctrl * ec_boards_ctrl;
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    ec_boards_ctrl = new Ec_Boards_ctrl(argv[1]); 
+
+    if ( ec_boards_ctrl->init() != EC_BOARD_OK) {
+        std::cout << "Error in boards init()... cannot proceed!" << std::endl;		
+        delete ec_boards_ctrl;
+        return 0;
+    }
+
+    Rid2PosMap  rid2pos = ec_boards_ctrl->get_Rid2PosMap();
+
+    const YAML::Node doc = ec_boards_ctrl->get_config_YAML_Node();
+    const YAML::Node firmware_update = doc["firmware_update"];
+
+    std::vector<int> slave_list;
+    bool use_rId = true;
+    
+    if ( firmware_update["slave_rId_list"] ) {
+        slave_list = firmware_update["slave_rId_list"].as<std::vector<int>>();
+    } else if ( firmware_update["slave_pos_list"] ) {
+        slave_list = firmware_update["slave_pos_list"].as<std::vector<int>>();
+        use_rId = false;
+    } else {
+        std::cout << "NO slave list found !!" << std::endl;
+        delete ec_boards_ctrl;
+        return 1;
+    }
+    
+    if ( slave_list.size() == 1 ) {
+        if ( slave_list[0] == 0 ) {
+            // all esc
+            use_rId = false;
+            slave_list.resize(ec_slavecount);
+            std::cout << "slave list size " << slave_list.size() << std::endl;
+            // exclude power board
+            // slave_list : [2 .. ec_slavecount)
+            std::iota(slave_list.begin(), slave_list.end(), 2);
+        
+        } else if ( slave_list[0] == -1 ) {
+            // big motor
+            use_rId = false;
+            slave_list.clear();
+            std::map<int, HpESC*> hp_boards;
+            ec_boards_ctrl->get_esc_map_bytype(HI_PWR_AC_MC, hp_boards);
+            for (auto it = hp_boards.begin(); it != hp_boards.end(); it++ ) {
+                slave_list.push_back(it->first);
+            }
+            ec_boards_ctrl->get_zombie_map_bytype(HI_PWR_AC_MC, hp_boards);
+            for (auto it = hp_boards.begin(); it != hp_boards.end(); it++ ) {
+                slave_list.push_back(it->first);
+            }
+        } else if ( slave_list[0] == -2 ) {
+            // med motor
+            use_rId = false;
+            slave_list.clear();
+            std::map<int, HpESC*> hp_boards;
+            ec_boards_ctrl->get_esc_map_bytype(HI_PWR_DC_MC, hp_boards);
+            for (auto it = hp_boards.begin(); it != hp_boards.end(); it++ ) {
+                slave_list.push_back(it->first);
+            }
+            ec_boards_ctrl->get_zombie_map_bytype(HI_PWR_DC_MC, hp_boards);
+            for (auto it = hp_boards.begin(); it != hp_boards.end(); it++ ) {
+                slave_list.push_back(it->first);
+            }
+        }
+    }
+    
+    std::string fw_path;
+    std::string bin_file;
+    int passwd;
+    EscWrapper * esc;
+    uint16_t   bType;
+    int sPos;
+
+    fw_path = firmware_update["fw_path"].as<std::string>();
+
+    for (auto it = slave_list.begin(); it != slave_list.end(); it++) {
+        if ( use_rId ) {
+            // use rId
+            try { sPos = rid2pos.at(*it); }
+            catch (const std::out_of_range& oor) { DPRINTF("%s\n", oor.what()); }
+        } else {
+            // use slave pos
+            sPos = *it;
+        }
+        esc = ec_boards_ctrl->slave_as_EscWrapper(sPos);
+        if ( !esc ) {
+            esc = ec_boards_ctrl->slave_as_Zombie(sPos);
+            DPRINTF("..... try with Z0mb13 %d\n", sPos);
+        }
+        if( esc ) {
+            YAML::Node motor_type;
+            bType = esc->get_ESC_type();
+            switch ( bType ) {
+                case HI_PWR_AC_MC :
+                    motor_type = firmware_update["big_motor"];
+                    break;
+                case HI_PWR_DC_MC :
+                    motor_type = firmware_update["medium_motor"];
+                    break;
+                case LO_PWR_DC_MC :
+                    motor_type = firmware_update["small_motor"];
+                    break;
+                case FT6 :
+                    motor_type = firmware_update["force_torque_6"];
+                    break;
+                case POW_BOARD  :
+                    motor_type = firmware_update["power_hub"];
+                    break;
+                default :
+                    break;
+            }
+
+            if ( ! motor_type.IsNull() ) {
+                bin_file    = motor_type["bin_file"].as<std::string>();
+                passwd      = motor_type["passwd"].as<int>();
+                DPRINTF("%d %s 0x%04X \n", *it, (fw_path+bin_file).c_str(), passwd);
+                ret = ec_boards_ctrl->update_board_firmware(sPos, fw_path+bin_file, passwd);
+                if ( ! ret  ) {
+                    DPRINTF("FAIL update slave pos %d\n" ,sPos);
+                }
+            } else {
+                DPRINTF("Unknown esc type %d\n" ,bType);
+            }
+
+        }
+    }
+    
+    delete ec_boards_ctrl;
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    ec_boards_ctrl = new Ec_Boards_ctrl(argv[1]); 
+
+    if ( ec_boards_ctrl->init() != EC_BOARD_OK) {
+        std::cout << "Error in boards init()... cannot proceed!" << std::endl;		
+        delete ec_boards_ctrl;
+        return 0;
+    }
+    
+    delete ec_boards_ctrl;
+
+    return 1;
+}
