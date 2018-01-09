42b7ee7c622bbf45151bbbfc490996f51529ed18
Alessio Margan <alessio.margan@iit.it>
2018-01-09 18:05:33 +0100

    wip on urdf

---
+++ examples/urdf_test/ec_boards_urdf_test.cpp
@@ -0,0 +1,179 @@
+#include <ec_boards_urdf_test.h>
+#include <iit/advr/centauro_robot_id.h>
+
+#include <linux/joystick.h>
+
+#include <spnav_config.h>
+#ifdef USE_X11
+#undef USE_X11
+#endif
+#include <spnav.h>
+
+#ifdef USE_LPMS_IMU
+#include <ImuData.h>
+#endif
+
+#define MID_POS(m,M)    (m+(M-m)/2)
+
+
+typedef struct js_event	js_input_t;
+typedef spnav_event	spnav_input_t;
+
+
+static const std::vector<double> Xt_2s = std::initializer_list<double> { 0, 2 };
+static const std::vector<double> Xt_3s = std::initializer_list<double> { 0, 3 };
+static const std::vector<double> Xt_4s = std::initializer_list<double> { 0, 4 };
+static const std::vector<double> Xt_5s = std::initializer_list<double> { 0, 5 };
+static const std::vector<double> Xt_9s = std::initializer_list<double> { 0, 9 };
+static const std::vector<double> Xt3_6s = std::initializer_list<double> { 0, 4, 6 };
+
+using namespace iit::ecat::advr;
+
+EC_boards_urdf::EC_boards_urdf ( const char* config_yaml ) :
+    Ec_Thread_Boards_base ( config_yaml ), URDF_adapter( ) {
+
+    name = "urd_test";
+    // not periodic
+    period.period = {0,1};
+
+#ifdef __COBALT__
+    schedpolicy = SCHED_FIFO;
+#else
+    schedpolicy = SCHED_OTHER;
+#endif
+    priority = sched_get_priority_max ( schedpolicy )-10;
+    stacksize = ECAT_PTHREAD_STACK_SIZE;
+
+    // open pipe ... xeno xddp or fifo
+    jsInXddp.init ( "EC_board_js_input" );
+    navInXddp.init ( "EC_board_nav_input" );
+    
+    init_urdf(get_config_YAML_Node()["ec_boards_base"]["urdf_config_file"].as<std::string>());
+         
+}
+
+EC_boards_urdf::~EC_boards_urdf() {
+
+}
+
+/*
+ *
+ */
+void EC_boards_urdf::init_preOP ( void ) {
+
+    Motor * moto;
+    int slave_pos;
+    float min_pos, max_pos, link_pos, motor_pos;
+   
+    //std::vector<int> pos_rid = centauro::robot_mcs_ids;
+    std::vector<int> pos_rid = get_urdf_rId();
+    std::vector<int> no_control = std::initializer_list<int> {
+        centauro::RA_HA,
+        centauro::LA_HA,
+    };
+       
+    remove_rids_intersection(pos_rid, no_control);
+
+    // !!!
+    get_esc_map_byclass ( motors_to_start,  pos_rid );
+    DPRINTF ( "found %lu <Motor> to_start >> %lu in urdf model\n", motors_to_start.size(), pos_rid.size() );
+
+    std::vector<double> Ys;
+    std::vector<double> Xs;
+
+    
+    for ( auto const& item : motors_to_start ) {
+    
+        slave_pos = item.first;
+        moto = item.second;
+        moto->readSDO ( "Min_pos", min_pos );
+        moto->readSDO ( "Max_pos", max_pos );
+        assert ( EC_WRP_OK == moto->readSDO ( "motor_pos", motor_pos ));
+        assert ( EC_WRP_OK == moto->readSDO ( "link_pos", link_pos ));
+        start_pos[slave_pos] = motor_pos; 
+        
+        //
+        Q(rid2Urdf(pos2Rid(slave_pos))) = DEG2RAD ( centauro::robot_ids_home_pos_deg.at(pos2Rid(slave_pos)) );
+        
+        // home
+        home[slave_pos] = DEG2RAD ( centauro::robot_ids_home_pos_deg.at(pos2Rid(slave_pos)) );
+
+        Ys =  std::initializer_list<double> { start_pos[slave_pos], home[slave_pos], home[slave_pos] };
+        spline_start2home[slave_pos] = std::make_shared<advr::Spline_trajectory> ( Xt3_6s, Ys );
+
+        //////////////////////////////////////////////////////////////////////////
+        // start controller :
+        // - read actual joint position and set as pos_ref
+        moto->start ( CTRL_SET_POS_MODE );
+    }
+
+    
+    DPRINTF ( ">>> wait xddp terminal ....\n" );
+    DPRINTF ( ">>> from another terminal run ec_master_test/scripts/xddp_term.py\n" );
+    char c; while ( termInXddp.xddp_read ( c ) <= 0 ) { osal_usleep(100); }  
+    
+    if ( motors_to_start.size() > 0 ) {
+        //
+        trj_queue.push_back ( spline_start2home );
+    }
+}
+
+
+void EC_boards_urdf::init_OP ( void ) {
+
+
+    try { advr::reset_trj ( trj_queue.at(0) ); }
+    catch ( const std::out_of_range &e ) {
+        throw std::runtime_error("Oh my gosh  ... trj_queue is empty !");
+    }    
+
+    
+    DPRINTF ( "End Init_OP\n" );
+    
+}
+
+int EC_boards_urdf::user_loop ( void ) {
+
+    static float ds;
+    static uint64_t count;
+    float spline_error = 0.07;
+
+    if ( ( count++ ) % 1000 == 0 ) {
+        DPRINTF ( "alive %ld\n", count/1000 );
+    }
+
+    ///////////////////////////////////////////////////////
+    //
+    if ( xddp_input ( ds ) > 0 ) {
+        DPRINTF ( ">> %f\n", ds );
+    }
+    
+    ///////////////////////////////////////////////////////
+    //
+    if ( ! trj_queue.empty() ) {
+        if ( go_there ( motors_to_start, trj_queue.at(0), spline_error, false) ) {
+            // running trj has finish ... remove from queue  !!
+            DPRINTF ( "running trj has finish ... remove from queue !!\n" );
+            trj_queue.pop_front();
+            try { advr::reset_trj ( trj_queue.at(0) ); }
+            catch ( const std::out_of_range &e ) {
+                // add trajectory ....
+            }
+        }
+    } else { // trj_queue is empty
+        
+    }
+
+}
+
+template<class C>
+int EC_boards_urdf::xddp_input ( C &user_cmd ) {
+
+    static int  bytes_cnt;
+    int         bytes = 0;
+
+
+    return bytes;
+}
+
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
---
+++ examples/urdf_test/include/ec_boards_urdf_test.h
@@ -0,0 +1,171 @@
+/*
+   Copyright (C) 2017 Italian Institute of Technology
+
+   Developer:
+       Alessio Margan (2017-, alessio.margan@iit.it)
+
+*/
+/**
+ *
+ * @author Alessio Margan (2017-, alessio.margan@iit.it)
+*/
+
+#ifndef __EC_BOARDS_URDF_H__
+#define __EC_BOARDS_URDF_H__
+
+#include <fstream>
+
+#include <iit/advr/ec_boards_base.h>
+#include <iit/advr/trajectory.h>
+
+#include <rbdl/rbdl.h>
+
+#ifndef RBDL_BUILD_ADDON_URDFREADER
+        #error "Error: RBDL addon URDFReader not enabled."
+#endif
+
+#include <rbdl/addons/urdfreader/urdfreader.h>
+
+
+namespace rbd = RigidBodyDynamics;
+using namespace RigidBodyDynamics::Math;
+
+/**
+ */
+
+class URDF_adapter {
+
+public:
+
+    URDF_adapter() {}
+    virtual ~URDF_adapter() { delete model; }
+    
+protected:
+
+    void init_urdf ( std::string urdf_config_yaml ) {
+        
+        rbdl_check_api_version (RBDL_API_VERSION);
+        model = new rbd::Model();
+        const YAML::Node config = YAML::LoadFile ( urdf_config_yaml );
+        std::string urdf_file = config["urdf_file"].as<std::string>();
+        if (!rbd::Addons::URDFReadFromFile ( urdf_file.c_str(), model, true, true)) {
+            std::cerr << "Error loading model " << std::endl;
+            abort();
+        }
+        
+        std::ifstream model_file (urdf_file);
+        if (!model_file) {
+            std::cerr << "Error opening file '" << urdf_file << "'." << std::endl;
+            abort();
+        }
+        
+        // reserve memory for the contents of the file
+        model_file.seekg(0, std::ios::end);
+        model_xml_string.reserve(model_file.tellg());
+        model_file.seekg(0, std::ios::beg);
+        model_xml_string.assign((std::istreambuf_iterator<char>(model_file)), std::istreambuf_iterator<char>());
+        model_file.close();
+        
+        DPRINTF("RDB::model DOF count %d\n", model->dof_count);
+        
+        Q = VectorNd::Zero (model->dof_count);
+        q_rbdl = VectorNd::Zero (model->dof_count);
+        QDot = VectorNd::Zero (model->dof_count);
+        Tau = VectorNd::Zero (model->dof_count);
+        QDDot = VectorNd::Zero (model->dof_count);
+        G = MatrixNd::Zero ( 6, model->dof_count + 6);
+        Jack = MatrixNd::Zero ( 3, model->dof_count);
+        
+        try {
+            urdf_joints_map = config["urdf_joints_map"].as<std::map<std::string,int>>();
+            for ( auto const& item : urdf_joints_map ) {
+                int id, q_id, rid;
+                const char * body_name = item.first.c_str();
+                id = model->GetBodyId(body_name);
+                q_id = model->mJoints[id].q_index;
+                rid = item.second;
+                rid2urdf[rid] = q_id;
+                urdf2rid[q_id] = rid;
+                std::cout << body_name << ": " << id << " " << q_id << " " << rid << std::endl;
+            }
+        } catch ( YAML::Exception &e ) {
+            DPRINTF ( "Catch Exception in %s ... %s\n", __PRETTY_FUNCTION__, e.what() );
+        }
+
+    }
+
+    std::map<std::string,int>   urdf_joints_map;
+    
+    rbd::Model * model;    
+    VectorNd Q;
+    VectorNd q_rbdl;
+    VectorNd QDot;
+    VectorNd QDDot;
+    VectorNd Tau;
+    MatrixNd G;
+    MatrixNd Jack;
+    
+    int rid2Urdf ( int rId ) {
+        //return rid2urdf.find ( rId ) != rid2urdf.end() ? rid2urdf[rId] : 0;
+        try { return rid2urdf.at(rId); } catch ( std::out_of_range &e ) {
+            throw iit::ecat::advr::EcBoardsError( iit::ecat::advr::EC_WRP_NOK, std::string("wrong robot Id ")+std::to_string(rId) );
+        }   
+    }
+    int urdf2Rid ( int uId ) {
+        //return urdf2rid.find ( uId ) != urdf2rid.end() ? urdf2rid[uId] : 0;
+        try { return urdf2rid.at(uId); } catch ( std::out_of_range &e ) {
+            throw iit::ecat::advr::EcBoardsError( iit::ecat::advr::EC_WRP_NOK, std::string("wrong urdf Id ")+std::to_string(uId) );
+        }   
+    }
+    
+    std::vector<int> get_urdf_rId() {
+        std::vector<int> rIds;
+        for ( auto const& item : urdf_joints_map ) { rIds.push_back(item.second); }
+        return rIds;
+    } 
+
+    std::string model_xml_string;
+        
+private:
+    
+    std::map<int, int> rid2urdf, urdf2rid;
+
+};
+
+
+
+class EC_boards_urdf : public Ec_Thread_Boards_base, URDF_adapter {
+public:
+
+
+    EC_boards_urdf ( const char * config_yaml );
+    virtual ~EC_boards_urdf();
+
+    template<class C>
+    int xddp_input ( C &user_cmd );
+    int user_loop ( void );
+
+private :
+
+    virtual void init_preOP ( void );
+    virtual void init_OP ( void );
+
+    XDDP_pipe jsInXddp, navInXddp;
+
+    advr::Trj_ptr_map spline_start2home;
+
+    std::map<int, iit::ecat::advr::Motor*>  left_arm;;
+    std::map<int, iit::ecat::advr::Motor*>  right_arm;
+    std::map<int, iit::ecat::advr::Motor*>  waist;
+
+    std::map<int, iit::ecat::advr::Motor*>  motors_to_start;
+    
+    // store reference to feed set_pos
+    std::map<std::string, double> q_map;
+};
+
+
+
+
+#endif
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
